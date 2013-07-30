e8a26dd8b84b09cd7a20f810c99cf3112967ec0f
Christian Fobel <christian@fobel.net>
2013-07-30 11:12:07 -0400

    Add 2d and 3d Fortran test function variants

--- vectorize_test/exts/lib/test_func-fortran-c-binding.f
+++ vectorize_test/exts/lib/test_func-fortran-c-binding.f
@@ -1,8 +1,33 @@
-      SUBROUTINE SQRT_TEST(A, B, N) BIND(c, name='f_sqrt_test')
+      SUBROUTINE SQRT_TEST(A, B, N) BIND(c, name='f_sqrt_test_1d')
       USE ISO_C_BINDING, ONLY: C_FLOAT, C_INT
       INTEGER(C_INT), INTENT(IN), VALUE :: N
       REAL(C_FLOAT), INTENT(IN) :: A(N)
       REAL(C_FLOAT), INTENT(OUT) :: B(N)
+
+      B = SQRT(A)
+
+      END SUBROUTINE
+
+
+      SUBROUTINE SQRT_TEST(A, B, M, N) BIND(c, name='f_sqrt_test_2d')
+      USE ISO_C_BINDING, ONLY: C_FLOAT, C_INT
+      INTEGER(C_INT), INTENT(IN), VALUE :: M
+      INTEGER(C_INT), INTENT(IN), VALUE :: N
+      REAL(C_FLOAT), INTENT(IN) :: A(M, N)
+      REAL(C_FLOAT), INTENT(OUT) :: B(M, N)
+
+      B = SQRT(A)
+
+      END SUBROUTINE
+
+
+      SUBROUTINE SQRT_TEST(A, B, K, M, N) BIND(c, name='f_sqrt_test_3d')
+      USE ISO_C_BINDING, ONLY: C_FLOAT, C_INT
+      INTEGER(C_INT), INTENT(IN), VALUE :: K
+      INTEGER(C_INT), INTENT(IN), VALUE :: M
+      INTEGER(C_INT), INTENT(IN), VALUE :: N
+      REAL(C_FLOAT), INTENT(IN) :: A(K, M, N)
+      REAL(C_FLOAT), INTENT(OUT) :: B(K, M, N)
 
       B = SQRT(A)
 
--- vectorize_test/exts/test_func.pyx
+++ vectorize_test/exts/test_func.pyx
@@ -7,7 +7,9 @@ cdef extern:
     void sqrt_test_1d(size_t size, float *a, float *b)
     void sqrt_test_2d(size_t size[2], float *a, float *b)
     void sqrt_test_3d(size_t size[3], float *a, float *b)
-    void f_sqrt_test(float *a, float *b, size_t size)
+    void f_sqrt_test_1d(float *a, float *b, size_t size)
+    void f_sqrt_test_2d(float *a, float *b, size_t M, size_t N)
+    void f_sqrt_test_3d(float *a, float *b, size_t K, size_t M, size_t N)
 
 
 ctypedef fused nd_view:
@@ -36,6 +38,19 @@ def py_c_sqrt_test_1d(float [:] a, float [:] b):
     sqrt_test_1d(N, &a[0], &b[0])
 
 
-def py_f_sqrt_test(float [:] a, float [:] b):
+def py_f_sqrt_test_3d(float [:, :, :] a, float [:, :, :] b):
+    cdef size_t K = a.shape[0]
+    cdef size_t M = a.shape[1]
+    cdef size_t N = a.shape[2]
+    f_sqrt_test_3d(&a[0, 0, 0], &b[0, 0, 0], K, M, N)
+
+
+def py_f_sqrt_test_2d(float [:, :] a, float [:, :] b):
+    cdef size_t M = a.shape[0]
+    cdef size_t N = a.shape[1]
+    f_sqrt_test_2d(&a[0, 0], &b[0, 0], M, N)
+
+
+def py_f_sqrt_test_1d(float [:] a, float [:] b):
     cdef size_t N = a.shape[0]
-    f_sqrt_test(&a[0], &b[0], N)
+    f_sqrt_test_1d(&a[0], &b[0], N)
